0ed2530281e624a7d64f0c69e718be712943255e
inform <inform@CLASS-02.kl9.uspu.ru>
2020-09-14 16:50:10 +0500

    Исправили

--- Dich/main.cc
+++ Dich/main.cc
@@ -1,11 +1,36 @@
 //подкючение заголовочных файлов
 #include<iostream>
+#include<iomanip>
+#include<cmath>
 
+//сама€ главна€ функци€
 int main()
 
 {
+	//выводим на консоль сообщение
 	std::cout <<
-			"Hello"<<
-			std::endl;
+			"Ѕудем табулировать синус"<< //сообщение
+			std::endl; //первые строки
+	//код исходных данных
+	std::cout <<"¬ведите начало и конец промежутка: ";
+	double start, end;
+	std::cin >>start>> end;
+
+	std::cout <<"¬ведите шаг: ";
+	double step;
+	std::cin >>step;
+
+	std::cout<<
+			" x | sin(x)\n"
+			"---------\n";
+	std::cout <<std::fixed;
+	for(double x = start; x <= end; x += step){
+		double y = sin(x);
+		std::cout <<
+				std::setw(7) <<std::setprecision(3) << x <<
+				'|' <<
+				std::setw(8) <<std::setprecision(4) << y <<
+				std::endl;
+	}
 	return 0;
 }
